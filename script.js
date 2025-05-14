7fad1831e81644f20150296ccc75517b724ab040
layon <locosilia5@gmail.com>
2025-05-13 21:34:19 -0300

    criando validação do cpf armazenando nas variaveis soma e resto

--- script.js
+++ script.js
@@ -4,5 +4,20 @@ function validarCPF(){
     if(cpf.length !== 11 || /^(\d)\1+$/.teste(cpf)){
         return false;
     }
-    
+
+    let soma = 0;
+    let resto;
+
+    // Validação do 1º DV
+
+    for(let i=1; i<=9; i++){
+        soma += parseInt(cpf.substring(i-1,i)) * (11 - i)
+        
+    }
+
+    resto=(soma*10) % 11;
+
+    if(resto === 10 || resto === 11){
+        resto = 0;
+    }
 }
