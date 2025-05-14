9dbb06c55bc0f9344ff24e672cf1ff388f07287e
layon <locosilia5@gmail.com>
2025-05-13 21:46:12 -0300

    terminamos o codigo de verificação do cpf

--- script.js
+++ script.js
@@ -20,4 +20,27 @@ function validarCPF(){
     if(resto === 10 || resto === 11){
         resto = 0;
     }
+    if(resto !== parseInt(cpf.substring(9,10))){
+        return false;
+    }
+    
+    // validação do 2º DV
+
+    soma = 0;
+
+    for(let i=1; i<=10; i++){
+        soma += parseInt(cpf.substring(i-1,i)) * (12-i)
+    }
+
+    resto = (soma*10) % 11;
+
+    if(resto === 10 || resto === 11){
+        resto = 0;
+    }
+
+    if(resto !== parseInt(cpf.substring(10,11))){
+        return false;
+    }
+
+    return true;
 }
